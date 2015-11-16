4ab8efc98fb04c75f8f14aa4dea7ddb2232d3de2
yuanfayang <739302606@qq.com>
2015-11-16 15:13:08 +0800

    初始提交

---
+++ weather-training/src/main/java/com/changhong/yuan/weather/App.java
@@ -0,0 +1,13 @@
+package com.changhong.yuan.weather;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
